004a4c5bdd949b08386ca1f5d1d405158afb3835
Brandon Sneed <brandon@redf.net>
2024-03-27 13:19:21 -0700

    Update version & location

--- Formula/swift-create-xcframework.rb
+++ Formula/swift-create-xcframework.rb
@@ -3,9 +3,9 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SwiftCreateXcframework < Formula
   desc "Creates an XCFramework out of a Swift Package using xcodebuild"
-  homepage "https://github.com/segmentio/swift-create-xcframework"
-  url "https://github.com/segmentio/swift-create-xcframework.git"
-  version "1.1.0"
+  homepage "https://github.com/segment-integrations/swift-create-xcframework"
+  url "https://github.com/segment-integrations/swift-create-xcframework.git"
+  version "2.4.0"
   sha256 "5c0773aed1aa0b4cfd0f6ca7e5e96a08719771ad5ed40294203d49d544a26fc7"
   license "MIT"
 
