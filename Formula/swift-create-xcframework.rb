56164db82546887e82f286011766094a328b5025
Brandon Sneed <brandon@redf.net>
2024-03-27 13:12:42 -0700

    Update sha

--- Formula/swift-create-xcframework.rb
+++ Formula/swift-create-xcframework.rb
@@ -6,7 +6,7 @@ class SwiftCreateXcframework < Formula
   homepage "https://github.com/segmentio/swift-create-xcframework"
   url "https://github.com/segmentio/swift-create-xcframework.git"
   version "1.1.0"
-  sha256 :no_check
+  sha256 "5c0773aed1aa0b4cfd0f6ca7e5e96a08719771ad5ed40294203d49d544a26fc7"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
